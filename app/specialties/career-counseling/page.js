f98bc9559df067e469512cce878b3cc45038f045
jose-escobedo <escobedo.jose48@outlook.com>
2025-06-15 21:59:37 -0700

    adding a career counseling oage with our page heading componentn.

---
+++ app/specialties/career-counseling/page.js
@@ -0,0 +1,16 @@
+import PageHeading from '@/components/PageHeading';
+import Link from 'next/link';
+
+export default function Specialties() {
+  return (
+    <>
+      <PageHeading
+        title="Career Counseling in Los Angeles"
+        description=""
+        bgColor="bg-bashbright"
+        tubeColors={['#b1876e', '#7a5e42']}
+      />
+
+    </>
+  );
+}
